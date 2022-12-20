6fe0742514f2a081227c8cee2f65846b8acce2ab
John Danielsson <JohnDanielsson@JDs-MacBook-Pro-work.local>
2022-12-19 20:35:55 -0800

    back to drawing board

--- ImageGatherer.py
+++ ImageGatherer.py
@@ -1,25 +1,25 @@
 class ImageGatherer:
-
+    pass
 
 # https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672&zoom=12&size=400x400&key=YOUR_API_KEY&signature=YOUR_SIGNATURE
-n_requests = 100
-apiRequestStart = 'https://maps.googleapis.com/maps/api/staticmap?'
-centerLat = 40.714728
-centerLon = -73.998672
-latDelta = []
-lonDelta = []
-# for i in range(n_requests):
-requests = []
-for dx in latDelta:
-    for dy in lonDelta:
-        requests.append(
-            makeRequest(
-                apiRequestStart + 
-                f'center={centerLat+dx},{centerLon+dy}'
-                'size=200x200' +
-                '&zoom=20'
-            )
-        )
+# n_requests = 100
+# apiRequestStart = 'https://maps.googleapis.com/maps/api/staticmap?'
+# centerLat = 40.714728
+# centerLon = -73.998672
+# latDelta = []
+# lonDelta = []
+# # for i in range(n_requests):
+# requests = []
+# for dx in latDelta:
+#     for dy in lonDelta:
+#         requests.append(
+#             makeRequest(
+#                 apiRequestStart + 
+#                 f'center={centerLat+dx},{centerLon+dy}'
+#                 'size=200x200' +
+#                 '&zoom=20'
+#             )
+#         )
 
-def makeRequest(apiRequest):
-    return ''
+# def makeRequest(apiRequest):
+#     return ''
